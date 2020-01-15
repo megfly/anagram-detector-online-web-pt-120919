1763a2ecbf5f384c1f0c0eedf64b369f0ef4b37d
Meghan Flynn <mflynn333@gmail.com>
2020-01-15 21:15:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def match (arr)
+    arr.keep_if { |str| name.split('').sort == str.split ('') }
+  end 
+  
+end 
